5d530a273f90220169e20d4fefbe995f9a49d3e5
abay-2002 <akbar121202@gmail.com>
2021-11-25 22:30:41 +0800

    twelveth commit

---
+++ 14.NodeJS-membuat-event/index.js
@@ -0,0 +1,10 @@
+// === NodeJs | Membuat event from scratch ===
+// Membuat event from scratch in NodeJs
+const {EventEmitter} = require('events');
+const myEvent = new EventEmitter();
+
+myEvent.on('lunch', () => {
+    console.log('yum');
+})
+
+myEvent.emit('lunch');
