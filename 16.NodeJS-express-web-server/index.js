1c4d6ef2745cba5fe174955df166a9b3674f41a4
abay-2002 <akbar121202@gmail.com>
2021-12-01 21:33:53 +0800

    fifteen commit

---
+++ 16.NodeJS-express-web-server/index.js
@@ -0,0 +1,30 @@
+// === NodeJS express web server ===
+// web server tanpa menggunakan express 
+
+// === web server menggunakan NPM Module express NodeJS === 
+const express = require('express')
+const app = express()
+const port = 8080
+
+// routes ke root.
+app.get('/', (req, res) => {
+    // res.send('Hello From ExpressJS');
+    res.sendFile('./pages/index.html', {root: __dirname});
+})
+
+// routes ke about.
+app.get('/about', (req, res) => {
+    // res.send('Hello From ExpressJS');
+    res.sendFile('./pages/about.html', {root: __dirname});
+})
+
+// routes
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send('404 Not found :(');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
